2656486bd3ecd8e7b641957a74b46dac360ba3c5
Umash Tatavarthy <umashd7@gmail.com>
2021-06-06 00:29:52 +0530

    Edit and Delete task confirmation

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -15,31 +15,37 @@ import firebase from 'firebase'
 
 
 function TodoList(props) {
-    
+
     const [open, setOpen] = React.useState(false);
-    const [checked,setChecked] = React.useState(false)
-   
+    const [checked, setChecked] = React.useState(false)
+
     const handleOpen = () => {
-        setOpen(true);
+        let text = ''
+        let res = ''
+        if(checked){
+            text =  "Wanna re-open the task ?" 
+            res = window.confirm(text);
+            if (res === true) {
+                setOpen(true)
+            }
+            else {
+                return
+            }
+        }
+        else setOpen(true)
+        
     };
 
     const handleClose = () => {
-        setOpen(false);       
+        setOpen(false);
     };
 
-    const checkBoxClicked = (e)=>{      
+    const checkBoxClicked = (e) => {
         let res = ''
-        let text = ''
-
-       if(checked === false){   
-            text  = "Wanna close the task ?"  ;                           
-        }
-        if(checked === true){        
-            text = "Wanna re-open the task ?"                             
-            }            
-        
+        let text = checked ? "Wanna re-open the task ?" : "Wanna close  the task ?"
         res = window.confirm(text);
-        if(res === true){
+
+        if (res === true) {
             setChecked(!checked)
         }
         else {
@@ -48,53 +54,63 @@ function TodoList(props) {
     }
 
     const deleteTodo = (e) => {
-        console.log('inside delete',props.user.uid)
-        
-        db.collection('todos').doc(props.user.uid).update({
-            userTodos : firebase.firestore.FieldValue.arrayRemove(props.todoObj)
-        },{  merge: true })  
-    }    
+        let text = "Are you sure to delete the task ?"
+        let res = ''
+        res = window.confirm(text);
+        if (res === true) {
+            db.collection('todos').doc(props.user.uid).update({
+                userTodos: firebase.firestore.FieldValue.arrayRemove(props.todoObj)
+            }, { merge: true })
+        }
+        else {
+            return
+        }
+        console.log('inside delete', props.user.uid)
+    }
+
     return (
         <div>
-            <DialogUpdate open = {open} handleOpen = {handleOpen} handleClose = {handleClose} todoObj = {props.todoObj} user = {props.user}  />
-                <List style={{marginTop: '1.5rem'}} >
-                <Grid container lg ='4'  alignContent ="stretch" spacing={0} justifyContent = "space-evenly" >
-                    <ListItem divider={true} disabled = {checked} style = {{color : 'aqua'}}>
-                        <TrendingFlatIcon/>
-                         <ListItemText primary={ props.todoObj.todo } style = {{ marginLeft: '10px' }} />
-                        
+
+            <DialogUpdate open={open} handleOpen={handleOpen} handleClose={handleClose} todoObj={props.todoObj} user={props.user} />
+            <List style={{ marginTop: '1.5rem' }} >
+                <Grid container lg='4' alignContent="stretch" spacing={0} justifyContent="space-evenly" >
+                    <ListItem divider={true} disabled={checked} style={{ color: 'white' }}>
+                        <TrendingFlatIcon />
+                        <ListItemText primary={props.todoObj.todo} style={{ marginLeft: '10px' }} />
+
                         <Checkbox style={{
-                             padding:'5px',
-                             marginLeft:'15px',
-                             marginRight:'0px',
-                             alignContent: 'center',
-                             color:'#64b5f6'
-                         }} checked = {checked} onClick = {checkBoxClicked}> 
-                         </Checkbox>
+                            padding: '5px',
+                            marginLeft: '15px',
+                            marginRight: '0px',
+                            alignContent: 'center',
+                            color: '#64b5f6'
+                        }} checked={checked} onClick={checkBoxClicked}>
+                        </Checkbox>
                         <EditIcon onClick={handleOpen}
                             style={{
-                            cursor:'pointer',
-                            padding:'5px',
-                            marginLeft:'15px',
-                            marginRight:'0px',
-                            alignContent: 'center'
-                        }}  />
-                       
-                         <DeleteIcon onClick={deleteTodo} style={{
-                             cursor: 'pointer',
-                             padding:'5px',
-                             marginLeft:'15px',
-                             marginRight:'0px',
-                             alignContent: 'center',
-                             color :'#ff5252'
-                         }} />
-                        
+                                cursor: 'pointer',
+                                padding: '5px',
+                                marginLeft: '15px',
+                                marginRight: '0px',
+                                alignContent: 'center',
+                                color : '#e4c620'
+                            }} />
+
+                        <DeleteIcon onClick={deleteTodo} style={{
+                            cursor: 'pointer',
+                            padding: '5px',
+                            marginLeft: '15px',
+                            marginRight: '0px',
+                            alignContent: 'center',
+                            color: '#ff5252'
+                        }} />
+
                     </ListItem>
-                   
-                    </Grid>
-                    
-                </List>
-        
+
+                </Grid>
+
+            </List>
+
         </div>
     )
 }
